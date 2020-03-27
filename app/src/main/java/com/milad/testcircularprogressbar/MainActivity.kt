063ba1fb930e66bc88dd0a59bf333665c8c488f6
miladsi90 <miladsalimi69@yahoo.com>
2020-03-27 21:32:02 +0430

    Remove some additional codes

--- app/src/main/java/com/milad/testcircularprogressbar/MainActivity.kt
+++ app/src/main/java/com/milad/testcircularprogressbar/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.milad.testcircularprogressbar
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.milad.customprogressbar.SolidCircularProgressbar
 
